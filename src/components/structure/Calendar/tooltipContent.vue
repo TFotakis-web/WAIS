443ba70426a17053549457f36b0fe83dcae941b9
TFotakis <fotakistzanis@gmail.com>
2020-11-05 01:32:43 +0200

    Created tooltipContent component for calendar

---
+++ src/components/structure/Calendar/tooltipContent.vue
@@ -0,0 +1,43 @@
+<template>
+	<div>
+		<p class="mb-1 font-weight-bold">{{ event.title }}</p>
+		<p class="mb-0 small">
+			<span v-if="!isAllDayEvent(event) && !isSingleDayEvent(event)">
+				{{ event.start | moment('DD/MM/YYYY HH:mm') }} - {{ event.end | moment('DD/MM/YYYY HH:mm') }}
+			</span>
+			<span v-else-if="!isAllDayEvent(event) && isSingleDayEvent(event)">
+				{{ event.start | moment('DD/MM/YYYY HH:mm') }} - {{ event.end | moment('HH:mm') }}
+			</span>
+			<span v-else-if="isAllDayEvent(event) && !isSingleDayEvent(event)">
+				{{ event.start | moment('DD/MM/YYYY') }} - {{ event.end | moment('DD/MM/YYYY') }}
+			</span>
+			<span v-else-if="isAllDayEvent(event) && isSingleDayEvent(event)">
+				{{ event.start | moment('DD/MM/YYYY') }}
+			</span>
+		</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'tooltipContent',
+		props: {
+			event: {
+				type: Object,
+				default: () => {},
+			},
+		},
+		methods: {
+			isAllDayEvent(event) {
+				const startTime = this.$moment(event.start).format('HH:mm:ss');
+				const endTime = this.$moment(event.end).format('HH:mm:ss');
+				return startTime === '00:00:00' && endTime === '23:59:59';
+			},
+			isSingleDayEvent(event) {
+				const startDate = this.$moment(event.start).format('YYYYMMDD');
+				const endDate = this.$moment(event.end).format('YYYYMMDD');
+				return startDate === endDate;
+			},
+		},
+	};
+</script>
