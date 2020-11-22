7926b92c7d200bc03972891a6290ca34bdfed1be
TFotakis <fotakistzanis@gmail.com>
2020-11-23 01:50:30 +0200

    localeDropdown.vue: Created & Implemented

---
+++ src/components/structure/localeDropdown.vue
@@ -0,0 +1,18 @@
+<template>
+	<mdb-dropdown tag="li" class="black-text">
+		<mdb-dropdown-toggle tag="a" navLink slot="toggle" waves-fixed class="pr-0">
+			<mdb-icon icon="globe" class="black-text" />
+		</mdb-dropdown-toggle>
+		<mdb-dropdown-menu style="min-width: unset">
+			<mdb-dropdown-item v-for="locale in $i18n.availableLocales" :key="locale" @click="$root.changeLanguage(locale)">{{
+				locale
+			}}</mdb-dropdown-item>
+		</mdb-dropdown-menu>
+	</mdb-dropdown>
+</template>
+
+<script>
+	export default {
+		name: 'LocaleDropdown',
+	};
+</script>
