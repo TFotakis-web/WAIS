ac581048c6715045f5a66b2637e80908bc467cf0
TFotakis <fotakistzanis@gmail.com>
2020-11-23 01:56:47 +0200

    auth.vue: Created & Implemented

---
+++ src/components/auth/auth.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<SignIn v-if="authPage === 'signIn'" @auth-page-changed="value => (authPage = value)" @auth-credentials="value => (credentials = value)"/>
+		<ForgotPassword v-else-if="authPage === 'forgotPassword'" @auth-page-changed="value => (authPage = value)" />
+		<SignUp v-else-if="authPage === 'signUp'" @auth-page-changed="value => (authPage = value)" @auth-credentials="value => (credentials = value)"/>
+		<ConfirmSignUp v-else-if="authPage === 'confirmSignUp'" @auth-page-changed="value => (authPage = value)" :credentials="credentials" />
+	</div>
+</template>
+
+<script>
+	import SignIn from '@/components/auth/signIn';
+	import ForgotPassword from '@/components/auth/forgotPassword';
+	import SignUp from '@/components/auth/signUp';
+	import ConfirmSignUp from '@/components/auth/confirmSignUp';
+	export default {
+		name: 'auth',
+		components: {
+			SignIn,
+			ForgotPassword,
+			SignUp,
+			ConfirmSignUp
+		},
+		data() {
+			return {
+				authPage: 'signIn',
+				credentials: {}
+			};
+		},
+	};
+</script>
