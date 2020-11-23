116f94eb8cb04ebbddac18b3b4cc6b7fd02ef375
TFotakis <fotakistzanis@gmail.com>
2020-11-23 05:20:29 +0200

    NotFound.vue: Created & Implemented

---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="text-center pt-5">
+		<h1 class="mb-3">
+			<strong>{{ $t('error404.header') }}</strong>
+		</h1>
+		<h4>
+			{{ $t('error404.message') }}
+			<router-link :to="{ name: 'Home' }">{{ $t('Home page') }}.</router-link>
+		</h4>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'NotFound',
+	};
+</script>
