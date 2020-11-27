cee92451b14d164b7d9201ea470f74c51ec3ea9e
TFotakis <fotakistzanis@gmail.com>
2020-11-27 20:18:40 +0200

    NotFound.vue: Update locales messages

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="text-center pt-5">
 		<h1 class="mb-3">
-			<strong>{{ $t('error404.header') }}</strong>
+			<strong>{{ $t('views.error404.header') }}</strong>
 		</h1>
 		<h4>
-			{{ $t('error404.message') }}
-			<router-link :to="{ name: 'Home' }">{{ $t('Home page') }}.</router-link>
+			{{ $t('views.error404.message') }}
+			<router-link :to="{ name: 'Home' }">{{ $t('views.homePage.pageTitle') }}.</router-link>
 		</h4>
 	</div>
 </template>
