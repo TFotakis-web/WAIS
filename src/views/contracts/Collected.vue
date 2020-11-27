253e1ffdf8154c94fd8b3f892906238180ad99d9
TFotakis <fotakistzanis@gmail.com>
2020-11-27 20:15:36 +0200

    Collected.vue: Update locales messages

--- src/views/contracts/Collected.vue
+++ src/views/contracts/Collected.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="text-center pt-5">
-		<h1>{{ $t('Collected') }}</h1>
-		<h5 class="text-info">{{ $t('Under Construction') }}</h5>
+		<h1>{{ $t('components.navigation.sidenav.contracts.collected') }}</h1>
+		<h5 class="text-info">{{ $t('various.underConstruction') }}</h5>
 	</div>
 </template>
 
