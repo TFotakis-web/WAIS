9e34715738a8d0cb5ab482f48c5f518ed1165bb8
TFotakis <fotakistzanis@gmail.com>
2020-12-08 01:05:12 +0200

    VehicleDetails.vue: Created & Initialized

---
+++ src/views/cards/VehicleDetails.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="text-center pt-5">
+		<h1>{{ $t('views.Vehicle Cards Details.pageTitle') }}</h1>
+		<h5 class="text-info">{{ $t('various.underConstruction') }}</h5>
+		<span>{{ vehicle }}</span>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'VehicleDetails',
+		data() {
+			return {
+				vehicle: {}
+			}
+		},
+		mounted () {
+			this.getVehicle();
+		},
+		methods: {
+			getVehicle: async function() {
+				this.vehicle = await this.$store.dispatch('vehicle/getVehicle', this.vehicleId);
+				console.log(this.vehicle);
+			}
+		},
+		computed: {
+			vehicleId() {
+				return this.$route.params.id;
+			}
+		},
+	};
+</script>
