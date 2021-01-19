2b64561e217fb656e8c0a5fc392bb8123bd83107
gstamatakis <giorgoshstam@gmail.com>
2021-01-19 15:31:30 +0200

    New account

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,53 +1,6 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const me = /* GraphQL */ `
-  query Me {
-    me {
-      Username
-      UserAttributes {
-        Name
-        Value
-      }
-      UserCreateDate
-      UserLastModifiedDate
-      Enabled
-      UserStatus
-      MFAOptions {
-        DeliveryMedium
-        AttributeName
-      }
-      PreferredMfaSetting
-      UserMFASettingList
-    }
-  }
-`;
-export const user = /* GraphQL */ `
-  query User($username: String) {
-    user(username: $username) {
-      Username
-      UserAttributes {
-        Name
-        Value
-      }
-      UserCreateDate
-      UserLastModifiedDate
-      Enabled
-      UserStatus
-      MFAOptions {
-        DeliveryMedium
-        AttributeName
-      }
-      PreferredMfaSetting
-      UserMFASettingList
-    }
-  }
-`;
-export const echo = /* GraphQL */ `
-  query Echo($msg: String) {
-    echo(msg: $msg)
-  }
-`;
 export const getTestType = /* GraphQL */ `
   query GetTestType($id: ID!) {
     getTestType(id: $id) {
