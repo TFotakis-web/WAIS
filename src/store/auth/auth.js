3c4ecedc5ec32ac31973d80588695ba8c0958ec5
TFotakis <fotakistzanis@gmail.com>
2021-01-30 15:56:47 +0200

    store/auth/auth.js: Implemented currentUserInfo fetching userProfile - Created updateUserAttributes

--- src/store/auth/auth.js
+++ src/store/auth/auth.js
@@ -5,7 +5,9 @@ import { getUserProfile } from '@/graphql/queries';
 export const auth = {
 	namespaced: true,
 	state: {
-		user: null
+		cognitoUser: null,
+		user: null,
+		userProfile: null,
 	},
 	mutations: {
 		setCognitoUser(state, payload) {
@@ -29,15 +31,20 @@ export const auth = {
 				return Promise.reject(error);
 			}
 		},
-		async signIn({ commit }, { username, password }) {
+		async signIn({ commit, dispatch }, { username, password }) {
 			try {
 				const cognitoUser = await Auth.signIn({
 					username,
 					password
 				});
-
-				const userInfo = await Auth.currentUserInfo();
-				commit("setUser", userInfo);
+				commit("setCognitoUser", cognitoUser);
+				if(cognitoUser.challengeName === 'NEW_PASSWORD_REQUIRED') {
+					return Promise.reject({
+						name: 'NEW_PASSWORD_REQUIRED',
+						code: 'NEW_PASSWORD_REQUIRED'
+					});
+				}
+				await dispatch("currentUserInfo");
 				return Promise.resolve("Success");
 			} catch (error) {
 				console.error(error);
@@ -115,16 +122,75 @@ export const auth = {
 		},
 		async currentUserInfo({ commit }) {
 			try {
+				commit('increaseGlobalPendingPromises', null, {root: true});
 				const userInfo = await Auth.currentUserInfo();
-				commit("setUser", userInfo);
+				commit('decreaseGlobalPendingPromises', null, {root: true});
+				if (userInfo) {
+					commit("setUser", userInfo);
+					commit('increaseGlobalPendingPromises', null, {root: true});
+					const userProfile = await API.graphql(graphqlOperation(getUserProfile, {id: userInfo.id}));
+					commit('decreaseGlobalPendingPromises', null, {root: true});
+					commit("setUserProfile", userProfile);
+				}
 				return Promise.resolve();
 			} catch (error) {
 				console.error(error);
 				return Promise.reject(error);
 			}
-		}
+		},
+		async currentAuthenticatedUser({ commit, dispatch }) {
+			try {
+				let cognitoUser = await Auth.currentAuthenticatedUser({
+					bypassCache: true  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+				}).catch(async error => {
+					console.log(error);
+					await Auth.signOut();
+				});
+				commit("setCognitoUser", cognitoUser);
+				await dispatch("currentUserInfo");
+				return Promise.resolve();
+			} catch (error) {
+				await Auth.signOut();
+				console.error(error);
+				return Promise.reject(error);
+			}
+		},
+		async updateUserAttributes({ dispatch }, attributes) {
+			try {
+				const user = await Auth.currentAuthenticatedUser();
+				// const attributes = {
+				// address: '',
+				// birthdate: '',
+				// email: '',
+				// family_name: '',
+				// gender: '',
+				// given_name: '',
+				// locale: '',
+				// middle_name: '',
+				// name: '',
+				// nickname: '',
+				// phone_number: '',
+				// picture: '',
+				// preferred_username: '',
+				// profile: '',
+				// website: '',
+				// zoneinfo: ''
+				// };
+				await Auth.updateUserAttributes(user, attributes);
+				dispatch("currentUserInfo");
+				return Promise.resolve();
+			} catch (error) {
+				console.error(error);
+				return Promise.reject(error);
+			}
+		},
 	},
 	getters: {
-		user: (state) => state.user
+		user: (state) => state.user,
+		userId: (state) => state.user.id,
+		username: (state) => state.user.username,
+		userAttributes: (state) => state.user.attributes,
+		userProfile: (state) => state.userProfile,
+		userPreferences: (state) => JSON.parse(state.userProfile.preferences),
 	}
 }
