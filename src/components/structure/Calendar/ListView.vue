6fea0ca1fbcf85f7f4555e20148c0e404ff6838f
TFotakis <fotakistzanis@gmail.com>
2021-02-03 22:58:34 +0200

    ListView.vue: Added scroll on overflow-y

--- src/components/structure/Calendar/ListView.vue
+++ src/components/structure/Calendar/ListView.vue
@@ -21,188 +21,179 @@
 						<mdb-btn color="info" darkWaves @click.native="changeView('list')">{{ $t('components.calendar.list') }}</mdb-btn>
 					</mdb-btn-group>
 				</div>
-				<table>
-					<tbody v-for="(event, index) in events" :key="index">
-						<tr
-							v-if="
-								event.start < new Date(firstDay.getFullYear(), firstDay.getMonth(), firstDay.getDate() + 7) &&
-									event.end >= new Date(firstDay.getFullYear(), firstDay.getMonth(), firstDay.getDate())
-							"
-							class="grey lighten-4"
-						>
+				<div style="height: 25em; overflow-y: scroll">
+					<table>
+						<tbody v-for="(event, index) in events" :key="index">
+						<tr v-if="event.start < new Date(firstDay.getFullYear(), firstDay.getMonth(), firstDay.getDate() + 7) && event.end >= new Date(firstDay.getFullYear(), firstDay.getMonth(), firstDay.getDate())" class="grey lighten-4">
 							<th v-if="event.start.toDateString() === event.end.toDateString()" class="font-weight-bold">
-								<mdb-icon icon="calendar-alt" class="pr-2" />
+								<mdb-icon icon="calendar-alt" class="pr-2"/>
 								{{ event.start.toLocaleDateString() }}
 							</th>
 							<th v-else class="font-weight-bold">
-								<mdb-icon icon="calendar-alt" class="pr-2" />
+								<mdb-icon icon="calendar-alt" class="pr-2"/>
 								{{ event.start.toLocaleDateString() }} -
 								{{ event.end.toLocaleDateString() }}
 							</th>
-							<th
-								v-if="event.start.toLocaleTimeString() === '00:00:00' && event.end.toLocaleTimeString() === '23:59:59'"
-								class="text-right font-weight-bold"
-							>
-								<mdb-icon icon="clock" class="pr-1" />all-day
+							<th v-if="event.start.toLocaleTimeString() === '00:00:00' && event.end.toLocaleTimeString() === '23:59:59'" class="text-right font-weight-bold">
+								<mdb-icon icon="clock" class="pr-1"/>
+								{{ $t('components.calendar.allDay') }}
 							</th>
 							<th v-else class="text-right font-weight-bold">
-								<mdb-icon icon="clock" class="pr-1" />
+								<mdb-icon icon="clock" class="pr-1"/>
 								{{ event.start.toLocaleTimeString() }} -
 								{{ event.end.toLocaleTimeString() }}
 							</th>
 						</tr>
-						<tr
-							v-if="
-								event.start < new Date(firstDay.getFullYear(), firstDay.getMonth(), firstDay.getDate() + 7) &&
-									event.end >= new Date(firstDay.getFullYear(), firstDay.getMonth(), firstDay.getDate())
-							"
-						>
+						<tr v-if=" event.start < new Date(firstDay.getFullYear(), firstDay.getMonth(), firstDay.getDate() + 7) && event.end >= new Date(firstDay.getFullYear(), firstDay.getMonth(), firstDay.getDate())">
 							<td colspan="2" class="mdb-list-event" @click="editEvent(index)">
-								<mdb-icon icon="circle" :color="event.color" class="pr-2" />
+								<mdb-icon icon="circle" :color="event.color" class="pr-2"/>
 								{{ event.title }}
 							</td>
 						</tr>
-					</tbody>
-				</table>
+						</tbody>
+					</table>
+				</div>
 			</div>
 		</mdb-card-body>
 	</mdb-card>
 </template>
-
 <script>
-	const mdbListView = {
-		name: 'mdbListView',
-		props: {
-			defaultDate: {
-				type: Date,
-			},
-			dayNames: {
-				type: Array,
-			},
-			monthNames: {
-				type: Array,
-			},
-			shortMonthNames: {
-				type: Array,
-			},
-			todayText: {
-				type: String,
-			},
-			events: {
-				type: [Array, Boolean],
-			},
-			firstDayOfWeek: {
-				type: Number,
-				default: 0,
-			},
+const mdbListView = {
+	name: 'mdbListView',
+	props: {
+		defaultDate: {
+			type: Date,
 		},
-		data() {
-			return {
-				firstDay: null,
-				lastDay: null,
-				title: null,
-			};
+		dayNames: {
+			type: Array,
 		},
-		created() {
-			this.date = this.defaultDate;
-			this.setFirstAndLastDayToDate(this.date);
+		monthNames: {
+			type: Array,
+		},
+		shortMonthNames: {
+			type: Array,
+		},
+		todayText: {
+			type: String,
+		},
+		events: {
+			type: [Array, Boolean],
+		},
+		firstDayOfWeek: {
+			type: Number,
+			default: 0,
+		},
+	},
+	data() {
+		return {
+			firstDay: null,
+			lastDay: null,
+			title: null,
+		};
+	},
+	created() {
+		this.date = this.defaultDate;
+		this.setFirstAndLastDayToDate(this.date);
+		this.updateTitle();
+	},
+	methods: {
+		changeView(view) {
+			this.$emit('changeView', view);
+		},
+		updateTitle() {
+			if (this.firstDay.getMonth() == this.lastDay.getMonth()) {
+				this.title =
+					this.firstDay.getDate() +
+					' - ' +
+					this.lastDay.getDate() +
+					' ' +
+					this.monthNames[this.firstDay.getMonth()] +
+					' ' +
+					this.firstDay.getFullYear();
+			} else if (this.firstDay.getFullYear() == this.lastDay.getFullYear()) {
+				this.title =
+					this.firstDay.getDate() +
+					' ' +
+					this.shortMonthNames[this.firstDay.getMonth()] +
+					' - ' +
+					this.lastDay.getDate() +
+					' ' +
+					this.shortMonthNames[this.lastDay.getMonth()] +
+					' ' +
+					this.firstDay.getFullYear();
+			} else {
+				this.title =
+					this.firstDay.getDate() +
+					' ' +
+					this.shortMonthNames[this.firstDay.getMonth()] +
+					' ' +
+					this.firstDay.getFullYear() +
+					' - ' +
+					this.lastDay.getDate() +
+					' ' +
+					this.shortMonthNames[this.lastDay.getMonth()] +
+					' ' +
+					this.lastDay.getFullYear();
+			}
+		},
+		setFirstAndLastDayToDate(date) {
+			let currentDayOfWeek = date.getDay();
+			if (currentDayOfWeek >= this.firstDayOfWeek) {
+				this.firstDay = new Date(date.getFullYear(), date.getMonth(), date.getDate() - currentDayOfWeek + this.firstDayOfWeek);
+			} else {
+				this.firstDay = new Date(date.getFullYear(), date.getMonth(), date.getDate() - 7 - currentDayOfWeek + this.firstDayOfWeek);
+			}
+			this.lastDay = new Date(this.firstDay.getFullYear(), this.firstDay.getMonth(), this.firstDay.getDate() + 6);
+		},
+		changeWeek(target) {
+			if (target === 'prev') {
+				this.firstDay = new Date(this.firstDay.getFullYear(), this.firstDay.getMonth(), this.firstDay.getDate() - 7);
+				this.lastDay = new Date(this.lastDay.getFullYear(), this.lastDay.getMonth(), this.lastDay.getDate() - 7);
+			}
+			if (target === 'next') {
+				this.firstDay = new Date(this.firstDay.getFullYear(), this.firstDay.getMonth(), this.firstDay.getDate() + 7);
+				this.lastDay = new Date(this.lastDay.getFullYear(), this.lastDay.getMonth(), this.lastDay.getDate() + 7);
+			}
+			if (target === 'today') {
+				this.setFirstAndLastDayToDate(new Date());
+			}
 			this.updateTitle();
+			this.$emit('dateChange', this.firstDay);
 		},
-		methods: {
-			changeView(view) {
-				this.$emit('changeView', view);
-			},
-			updateTitle() {
-				if (this.firstDay.getMonth() == this.lastDay.getMonth()) {
-					this.title =
-						this.firstDay.getDate() +
-						' - ' +
-						this.lastDay.getDate() +
-						' ' +
-						this.monthNames[this.firstDay.getMonth()] +
-						' ' +
-						this.firstDay.getFullYear();
-				} else if (this.firstDay.getFullYear() == this.lastDay.getFullYear()) {
-					this.title =
-						this.firstDay.getDate() +
-						' ' +
-						this.shortMonthNames[this.firstDay.getMonth()] +
-						' - ' +
-						this.lastDay.getDate() +
-						' ' +
-						this.shortMonthNames[this.lastDay.getMonth()] +
-						' ' +
-						this.firstDay.getFullYear();
-				} else {
-					this.title =
-						this.firstDay.getDate() +
-						' ' +
-						this.shortMonthNames[this.firstDay.getMonth()] +
-						' ' +
-						this.firstDay.getFullYear() +
-						' - ' +
-						this.lastDay.getDate() +
-						' ' +
-						this.shortMonthNames[this.lastDay.getMonth()] +
-						' ' +
-						this.lastDay.getFullYear();
-				}
-			},
-			setFirstAndLastDayToDate(date) {
-				let currentDayOfWeek = date.getDay();
-				if (currentDayOfWeek >= this.firstDayOfWeek) {
-					this.firstDay = new Date(date.getFullYear(), date.getMonth(), date.getDate() - currentDayOfWeek + this.firstDayOfWeek);
-				} else {
-					this.firstDay = new Date(date.getFullYear(), date.getMonth(), date.getDate() - 7 - currentDayOfWeek + this.firstDayOfWeek);
-				}
-				this.lastDay = new Date(this.firstDay.getFullYear(), this.firstDay.getMonth(), this.firstDay.getDate() + 6);
-			},
-			changeWeek(target) {
-				if (target === 'prev') {
-					this.firstDay = new Date(this.firstDay.getFullYear(), this.firstDay.getMonth(), this.firstDay.getDate() - 7);
-					this.lastDay = new Date(this.lastDay.getFullYear(), this.lastDay.getMonth(), this.lastDay.getDate() - 7);
-				}
-				if (target === 'next') {
-					this.firstDay = new Date(this.firstDay.getFullYear(), this.firstDay.getMonth(), this.firstDay.getDate() + 7);
-					this.lastDay = new Date(this.lastDay.getFullYear(), this.lastDay.getMonth(), this.lastDay.getDate() + 7);
-				}
-				if (target === 'today') {
-					this.setFirstAndLastDayToDate(new Date());
-				}
-				this.updateTitle();
-				this.$emit('dateChange', this.firstDay);
-			},
-			editEvent(eventIndex) {
-				this.$emit('editEvent', eventIndex);
-			},
+		editEvent(eventIndex) {
+			this.$emit('editEvent', eventIndex);
 		},
-	};
+	},
+};
 
-	export default mdbListView;
-	export { mdbListView };
+export default mdbListView;
+export {mdbListView};
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-	.mdb-list-view {
-		width: 100%;
-		height: 100%;
-		/* margin-bottom: 50px; */
-	}
-	.mdb-list-view table {
-		width: 100%;
-		border: 1px solid #ddd;
-	}
-	.mdb-list-view table tr td,
-	.mdb-list-view table tr th {
-		padding: 8px 10px;
-		border-top: 1px solid #ddd;
-		border-bottom: 1px solid #ddd;
-	}
-	.mdb-list-event {
-		cursor: pointer;
-	}
-	.mdb-list-event:hover {
-		background-color: rgba(69, 82, 110, 0.05);
-	}
+.mdb-list-view {
+	width: 100%;
+	height: 100%;
+	/* margin-bottom: 50px; */
+}
+
+.mdb-list-view table {
+	width: 100%;
+	border: 1px solid #ddd;
+}
+
+.mdb-list-view table tr td,
+.mdb-list-view table tr th {
+	padding: 8px 10px;
+	border-top: 1px solid #ddd;
+	border-bottom: 1px solid #ddd;
+}
+
+.mdb-list-event {
+	cursor: pointer;
+}
+
+.mdb-list-event:hover {
+	background-color: rgba(69, 82, 110, 0.05);
+}
 </style>
