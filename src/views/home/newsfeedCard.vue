af8d3e5d28eb6826bd78501802a36689b56d5c55
TFotakis <fotakistzanis@gmail.com>
2021-02-09 20:21:24 +0200

    Added home folder and its views/components

---
+++ src/views/home/newsfeedCard.vue
@@ -0,0 +1,67 @@
+<template>
+	<mdb-card cascade narrow>
+		<mdb-view cascade class="gradient-card-header blue">
+			<h5 class="mb-0">News Feed</h5>
+		</mdb-view>
+		<mdb-card-body>
+			<mdb-list-group :style="{'min-height': maxContentHeight + 'px', 'max-height': maxContentHeight + 'px'}" style="overflow-y: scroll">
+				<mdb-list-group-item v-for="item in news" :key="'newsItems' + item.id.toString()">
+					<div>
+						<div class="d-flex w-100 justify-content-between">
+							<h5 class="mb-2 h5">{{ item.heading }}</h5>
+							<small>{{ item.releaseDate }}</small>
+						</div>
+						<p class="mb-2">{{ item.middleText }}</p>
+						<small>{{ item.lowerText }}</small>
+					</div>
+				</mdb-list-group-item>
+			</mdb-list-group>
+		</mdb-card-body>
+	</mdb-card>
+</template>
+<script>
+export default {
+	name: "newsfeedCard",
+	props: {
+		maxContentHeight: {
+			type: Number,
+			default: 300
+		},
+	},
+	data() {
+		return {
+			news: []
+		}
+	},
+	mounted() {
+		this.getNews();
+	},
+	methods: {
+		getNews() {
+			this.news = [
+				{
+					id: 0,
+					heading: 'List group item heading',
+					releaseDate: '3 days ago',
+					middleText: 'Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.',
+					lowerText: 'Donec id elit non mi porta.',
+				},
+				{
+					id: 1,
+					heading: 'List group item heading',
+					releaseDate: '3 days ago',
+					middleText: 'Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.',
+					lowerText: 'Donec id elit non mi porta.',
+				},
+				{
+					id: 2,
+					heading: 'List group item heading',
+					releaseDate: '3 days ago',
+					middleText: 'Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.',
+					lowerText: 'Donec id elit non mi porta.',
+				},
+			];
+		}
+	},
+}
+</script>
---
+++ src/views/home/performanceCard.vue
@@ -0,0 +1,86 @@
+<template>
+	<mdb-card cascade narrow>
+		<mdb-view cascade class="gradient-card-header blue">
+			<h5 class="mb-0">Performance</h5>
+		</mdb-view>
+		<mdb-card-body>
+			<div v-if="!show" :style="{'min-height': maxContentHeight}"></div>
+			<mdb-line-chart v-if="show" :data="lineChartData" :options="lineChartOptions" :height="maxContentHeight"/>
+		</mdb-card-body>
+	</mdb-card>
+</template>
+<script>
+export default {
+	name: "performanceCard",
+	data() {
+		return {
+			show: false,
+			lineChartData: {
+				labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+				datasets: [
+					{
+						label: 'Ασφάλειες Αυτοκινήτου (‎€)',
+						backgroundColor: 'rgba(255, 99, 132, 0.1)',
+						borderColor: 'rgba(255, 99, 132, 1)',
+						borderWidth: 0.7,
+						data: [6500, 5900, 8000, 8100, 5600, 5500, 4000],
+					},
+					{
+						label: 'Ασφάλειες Ζωής (‎€)',
+						backgroundColor: 'rgba(151,187,205,0.2)',
+						borderColor: 'rgba(151,187,205,1)',
+						borderWidth: 0.8,
+						data: [2800, 4800, 4000, 1900, 8600, 2700, 9000],
+					},
+				],
+			},
+			lineChartOptions: {
+				responsive: true,
+				maintainAspectRatio: false,
+				scales: {
+					xAxes: [
+						{
+							gridLines: {
+								display: true,
+								color: 'rgba(0, 0, 0, 0.1)',
+							},
+							ticks: {
+								fontColor: 'black',
+							},
+						},
+					],
+					yAxes: [
+						{
+							gridLines: {
+								display: true,
+								color: 'rgba(0, 0, 0, 0.1)',
+							},
+							ticks: {
+								fontColor: 'black',
+							},
+						},
+					],
+				},
+				legend: {
+					labels: {
+						fontColor: 'black',
+					},
+				},
+			},
+		}
+	},
+	props: {
+		maxContentHeight: {
+			type: Number,
+			default: 300
+		},
+	},
+	mounted() {
+		// this.show = true;
+
+		setTimeout(() => {
+			this.show = true;
+		}, 3000);
+	}
+}
+</script>
