6ac6d19ad9865861dad0f307f8f9f63fd08362cc
TFotakis <fotakistzanis@gmail.com>
2021-02-23 19:17:21 +0200

    store/index.js: Added echo action

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import { auth } from '@/store/auth/auth';
+import { API, graphqlOperation } from "aws-amplify";
+import { echo } from "@/graphql/queries";
 // import { vehicle } from '@/store/vehicle/vehicle';
 // import { trade } from '@/store/trade/trade';
 // import { platformData } from '@/store/platformData/platformData';
@@ -41,7 +43,21 @@ export default new Vuex.Store({
 			state.localesLoaded = loaded;
 		}
 	},
-	actions: {},
+	actions: {
+		echo() {
+			return new Promise((resolve, reject) => {
+				API.graphql(graphqlOperation(echo, { msg: 'Hello from echo' }))
+					.then((response) => {
+						console.log('Echo response:', response);
+						resolve();
+					})
+					.catch((error) => {
+						console.error(error);
+						reject(error);
+					});
+			});
+		}
+	},
 	getters: {
 		globalPendingPromises: (state) => state.globalPendingPromises,
 		routerViewPendingPromises: (state) => state.routerViewPendingPromises,
