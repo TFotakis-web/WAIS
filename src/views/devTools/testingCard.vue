1d7791174fc002758af82b6fb5d2087ca4a92407
TFotakis <fotakistzanis@gmail.com>
2021-02-25 02:23:43 +0200

    testingCard.vue: Added me test func

--- src/views/devTools/testingCard.vue
+++ src/views/devTools/testingCard.vue
@@ -8,7 +8,6 @@
 				<mdb-btn @click.native="testAll" :disabled="testAllDisabled" outline="primary">Test All</mdb-btn>
 				<span>{{ testAllMessage }}</span>
 			</div>
-
 			<div v-for="(test, testName) in tests" :key="testName">
 				<mdb-btn @click.native="test.testFunc" :disabled="test.disabled" color="primary">{{ testName }}</mdb-btn>
 				<span>{{ test.message }}</span>
@@ -17,63 +16,79 @@
 	</mdb-card>
 </template>
 <script>
-export default {
-	name: "testingCard",
-	data() {
-		return {
-			testAllMessage: '',
-			testAllDisabled: false,
-			tests: {
-        'Template Test': {
-          message: '',
-          disabled: false,
-          testFunc: () => {
-            this.tests['Template Test'].disabled = true;
-            this.tests['Template Test'].message = 'Running...';
+	export default {
+		name: "testingCard",
+		data() {
+			return {
+				testAllMessage: '',
+				testAllDisabled: false,
+				tests: {
+					'Template Test': {
+						message: '',
+						disabled: false,
+						testFunc: () => {
+							this.tests['Template Test'].disabled = true;
+							this.tests['Template Test'].message = 'Running...';
 
-            console.log("Test Passed!");
+							console.log("Test Passed!");
 
-            this.tests['Template Test'].message = 'OK';
-            this.tests['Template Test'].disabled = false;
-            return 0;
-          }
-        },
-        'Echo': {
-          message: '',
-          disabled: false,
-          testFunc: () => {
-            this.tests['Echo'].disabled = true;
-            this.tests['Echo'].message = 'Running...';
+							this.tests['Template Test'].message = 'OK';
+							this.tests['Template Test'].disabled = false;
+							return 0;
+						}
+					},
+					'Echo': {
+						message: '',
+						disabled: false,
+						testFunc: () => {
+							this.tests['Echo'].disabled = true;
+							this.tests['Echo'].message = 'Running...';
 
-            this.$store.dispatch('echo');
+							this.$store.dispatch('echo');
 
-            console.log("Test Passed!");
+							console.log("Test Passed!");
 
-            this.tests['Echo'].message = 'OK';
-            this.tests['Echo'].disabled = false;
-            return 0;
-          }
-        },
+							this.tests['Echo'].message = 'OK';
+							this.tests['Echo'].disabled = false;
+							return 0;
+						}
+					},
+					'Me': {
+						message: '',
+						disabled: false,
+						testFunc: () => {
+							this.tests['Me'].disabled = true;
+							this.tests['Me'].message = 'Running...';
+
+							this.$store.dispatch('me');
+
+							console.log("Test Passed!");
+
+							this.tests['Me'].message = 'OK';
+							this.tests['Me'].disabled = false;
+							return 0;
+						}
+					},
+				}
 			}
-		}
-	},
-	methods: {
-		testAll() {
-			this.testAllDisabled = true;
-			let errorCount = 0;
-			for(let test in this.tests) {
-				this.testAllMessage = 'Running ' + test;
-				errorCount += this.tests[test].testFunc();
+		},
+		methods: {
+			testAll() {
+				this.testAllDisabled = true;
+				let errorCount = 0;
+				for (let test in this.tests) {
+					this.testAllMessage = 'Running ' + test;
+					errorCount += this.tests[test].testFunc();
+				}
+				if (errorCount !== 0) {
+					this.testAllMessage = 'Errors in ' + errorCount.toString() + 'tests';
+				} else {
+					this.testAllMessage = 'OK';
+				}
+				this.testAllDisabled = false;
 			}
-			if(errorCount !== 0) {
-				this.testAllMessage = 'Errors in ' + errorCount.toString() + 'tests';
-			} else {
-				this.testAllMessage = 'OK';
-			}
-			this.testAllDisabled = false;
-		}
-	},
-}
+		},
+	}
 </script>
 <style scoped>
 </style>
