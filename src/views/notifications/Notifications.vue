4e281a54168983b8f73308e6133875ce279ea14c
TFotakis <fotakistzanis@gmail.com>
2021-03-03 20:52:16 +0200

    Notifications.vue: Added simple UI for testing

--- src/views/notifications/Notifications.vue
+++ src/views/notifications/Notifications.vue
@@ -3,6 +3,17 @@
 		<h1>{{ $t('views.notifications.pageTitle') }}</h1>
 		<h5 class="text-info mb-5">{{ $t('various.underConstruction') }}</h5>
 		<mdb-container>
+			<mdb-card>
+				<mdb-card-body>
+					<mdb-list-group>
+						<mdb-list-group-item v-for="request in createTradeList" :key="request.id">
+							<span>{{ request.payload.surname + ' ' + request.payload.name + ' ' + request.payload.fathersName + ' | ' + request.payload.tradeName }}</span>
+							<mdb-btn @click.native="acceptRequest(request)" color="success" size="sm" class="my-0">{{ $t('actions.accept') }}</mdb-btn>
+							<mdb-btn @click.native="rejectRequest(request)" color="danger" size="sm" class="my-0">{{ $t('actions.reject') }}</mdb-btn>
+						</mdb-list-group-item>
+					</mdb-list-group>
+				</mdb-card-body>
+			</mdb-card>
 		</mdb-container>
 	</div>
 </template>
@@ -11,16 +22,39 @@
 
 	export default {
 		name: "Notifications",
+		data() {
+			return {
+				requests: [],
+			}
+		},
 		mounted() {
 			this.listRequestsByReceiverEmail("wais@admin.com")
-			.then((response) => {
-				console.log(response);
-			});
+				.then((response) => {
+					for (let request of response) {
+						request.payload = JSON.parse(request.payload);
+					}
+					this.requests = response;
+				});
 		},
 		methods: {
-			...mapActions('request', ['listRequestsByReceiverEmail'])
+			...mapActions('request', ['listRequestsByReceiverEmail', 'resolveRequest']),
+			acceptRequest(request) {
+				this.resolveRequest({
+					id: request.id,
+					payload: { decision: 'ACCEPT' }
+				});
+			},
+			rejectRequest(request) {
+				this.resolveRequest({
+					id: request.id,
+					payload: { decision: 'REJECT' }
+				});
+			}
 		},
+		computed: {
+			createTradeList() {
+				return this.requests.filter(el => el.type === 'CREATE_TRADE');
+			}
+		}
 	}
 </script>
-<style scoped>
-</style>
