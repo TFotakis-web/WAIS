175c99de8783c78c496f15b95cb90be6a8a6270a
TFotakis <fotakistzanis@gmail.com>
2021-03-13 20:45:13 +0200

    signIn.vue: Added password visibility toggle

--- src/components/auth/signIn.vue
+++ src/components/auth/signIn.vue
@@ -8,23 +8,24 @@
 		<mdb-card-body>
 			<form @submit.prevent="signIn">
 				<mdb-input v-model="credentials.username" :label="$t('fields.username')" icon="user-circle" type="text" required class="mb-4" name="username" autocomplete="username" outline/>
-				<mdb-input v-model="credentials.password" :label="$t('fields.password')" icon="lock" type="password" required class="mb-3" name="password" autocomplete="current-password" outline/>
-				<span @click="$emit('auth-page-changed', 'forgotPassword')" class="a-tag">{{ $t('views.auth.forgotYourPassword') }}</span>
+				<mdb-input v-model="credentials.password" :label="$t('fields.password')" icon="lock" :type="passwordVisible ? 'text' : 'password'" required class="mb-3" name="password" autocomplete="current-password" outline>
+					<mdb-btn @click.native="passwordVisible = !passwordVisible" :icon="passwordVisible ? 'eye-slash' : 'eye'" flat slot="append" class="p-0"/>
+				</mdb-input>
+				<span @click="$emit('auth-page-changed', 'forgotPassword')" class="a-tag d-block">{{ $t('views.auth.forgotYourPassword') }}</span>
 				<div class="text-center">
-					<loadingBtn color="primary" type="submit" :rounded="true" :loading="loading" :text="$t('views.auth.signIn')" :loadingText="$t('views.auth.signingIn')" class="my-3" />
+					<loadingBtn color="primary" type="submit" :rounded="true" :loading="loading" :text="$t('views.auth.signIn')" :loadingText="$t('views.auth.signingIn')" class="my-3"/>
 					<p v-if="error !== {}" class="text-danger">{{ error.message }}</p>
-					<hr />
+					<hr/>
 					<p class="mt-4">
 						<span>{{ $t('views.auth.dontHaveAnAccount') }} </span>
 						<span @click="$emit('auth-page-changed', 'signUp')" class="a-tag">{{ $t('views.auth.createAccount') }}</span>
 					</p>
-					<localeDropdown />
+					<localeDropdown/>
 				</div>
 			</form>
 		</mdb-card-body>
 	</mdb-card>
 </template>
-
 <script>
 	import { mapActions } from 'vuex';
 	import localeDropdown from '@/components/structure/localeDropdown';
@@ -44,13 +45,14 @@
 					password: '',
 				},
 				error: {},
+				passwordVisible: false,
 			};
 		},
 		methods: {
 			...mapActions({
 				signInStore: 'auth/signIn',
 			}),
-			signIn: async function() {
+			signIn: async function () {
 				this.loading = true;
 				this.error = {};
 				try {
