e0ae9f8ea695ed395e6dc12e7573cebd6239ff63
TFotakis <fotakistzanis@gmail.com>
2021-03-19 16:52:26 +0200

    AuthPage.vue: Removed auth component

--- src/views/auth/AuthPage.vue
+++ src/views/auth/AuthPage.vue
@@ -1,21 +1,18 @@
 <template>
 	<ion-page>
 		<ion-content>
-			<h1>Login</h1>
-			<auth/>
+			<router-view/>
 		</ion-content>
 	</ion-page>
 </template>
 <script>
 	import { IonPage, IonContent } from '@ionic/vue';
-	import auth from '@/components/auth/auth.vue';
 
 	export default {
 		name: 'AuthPage',
 		components: {
 			IonPage,
 			IonContent,
-			auth,
 		},
 	};
 </script>
