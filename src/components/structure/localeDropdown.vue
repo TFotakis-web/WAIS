668584e3343ad0d07873fb55cbb1aad1f4980d97
TFotakis <fotakistzanis@gmail.com>
2021-03-22 03:07:03 +0200

    localeDropdown.vue: Migrated to Ionic

--- src/components/structure/localeDropdown.vue
+++ src/components/structure/localeDropdown.vue
@@ -1,20 +1,38 @@
 <template>
-	<mdb-tooltip material trigger="hover" :options="{ placement: 'left' }">
-		<span slot="tip">{{ $t('components.localeDropdown.language') }}</span>
-		<mdb-dropdown slot="reference" tag="li" class="black-text">
-			<mdb-dropdown-toggle tag="a" navLink slot="toggle" waves-fixed class="pr-0" style="overflow: unset">
-				<mdb-icon icon="globe" class="black-text"/>
-			</mdb-dropdown-toggle>
-			<mdb-dropdown-menu style="min-width: unset">
-				<mdb-dropdown-item v-for="locale in $i18n.availableLocales" :key="locale" @click="$i18n.$loadLanguageAsync(locale)">
-					{{ locale }}
-				</mdb-dropdown-item>
-			</mdb-dropdown-menu>
-		</mdb-dropdown>
-	</mdb-tooltip>
+	<ion-item>
+		<ion-icon :icon="ionicons.languageOutline" slot="start" class="ion-align-self-center"/>
+		<ion-label>{{ $t('components.localeDropdown.language') }}</ion-label>
+		<ion-select :ok-text="$t('actions.ok')" :cancel-text="$t('actions.cancel')" interface="popover" :ionChange="$i18n.$loadLanguageAsync(selectedLocale)" v-model="selectedLocale">
+			<ion-select-option v-for="locale in $i18n.availableLocales" :key="locale" :value="locale">{{ locale }}</ion-select-option>
+		</ion-select>
+	</ion-item>
 </template>
 <script>
+	import { languageOutline } from 'ionicons/icons';
+	import {
+		IonItem,
+		IonIcon,
+		IonLabel,
+		IonSelect,
+		IonSelectOption,
+	} from '@ionic/vue';
+
 	export default {
 		name: 'LocaleDropdown',
+		components: {
+			IonItem,
+			IonIcon,
+			IonLabel,
+			IonSelect,
+			IonSelectOption,
+		},
+		data() {
+			return {
+				selectedLocale: this.$i18n.locale,
+				ionicons: {
+					languageOutline,
+				},
+			};
+		},
 	};
 </script>
