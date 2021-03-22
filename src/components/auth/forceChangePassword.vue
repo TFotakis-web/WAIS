b3d6b2a483f144dc3565d14a13202799616ed55c
TFotakis <fotakistzanis@gmail.com>
2021-03-22 02:06:28 +0200

    forceChangePassword.vue: Migrated to Ionic

--- src/components/auth/forceChangePassword.vue
+++ src/components/auth/forceChangePassword.vue
@@ -1,37 +1,74 @@
 <template>
-	<mdb-card>
-		<mdb-card-header class="blue-gradient m-0">
-			<mdb-row class="d-flex justify-content-center">
-				<h3 class="white-text m-0 p-5 font-weight-bold">{{ $t('views.auth.changeYourPassword') }}</h3>
-			</mdb-row>
-		</mdb-card-header>
-		<mdb-card-body>
-			<form @submit.prevent="completeNewPassword">
-				<mdb-input v-model="newPassword" :label="$t('fields.newPassword')" icon="lock" :type="passwordVisible ? 'text' : 'password'" required class="mb-3" name="password" autocomplete="new-password" outline>
-					<mdb-btn @click="passwordVisible = !passwordVisible" :icon="passwordVisible ? 'eye-slash' : 'eye'" flat slot="append" class="p-0"/>
-				</mdb-input>
-				<div class="text-center">
-					<loadingBtn color="primary" type="submit" :rounded="true" :loading="loading" :text="$t('actions.submit')" :loadingText="$t('actions.submitting')" class="my-3"/>
-					<p v-if="error !== {}" class="text-danger">{{ error.message }}</p>
-					<hr/>
-					<p class="mt-4">
-						<span @click="$emit('auth-page-changed', 'signIn')" class="a-tag">{{ $t('views.auth.backToSignIn') }}</span>
-					</p>
-					<localeDropdown/>
-				</div>
-			</form>
-		</mdb-card-body>
-	</mdb-card>
+	<ion-grid fixed>
+		<ion-row class="ion-justify-content-center">
+			<ion-col size-sm="10" size-md="8" size-lg="6" size-xl="6">
+				<ion-card>
+					<ion-card-header>
+						<ion-card-title>{{ $t('views.auth.changeYourPassword') }}</ion-card-title>
+					</ion-card-header>
+					<ion-card-content>
+						<form @submit.prevent="completeNewPassword">
+							<ion-item>
+								<ion-icon :icon="ionicons.keyOutline" slot="start" class="ion-align-self-center"/>
+								<ion-label position="floating">{{ $t('fields.newPassword') }}</ion-label>
+								<ion-input v-model="newPassword" :type="passwordVisible ? 'text' : 'password'" name="password" autocomplete="new-password" required/>
+								<ion-button @click="passwordVisible = !passwordVisible" slot="end" fill="clear" class="ion-align-self-center">
+									<ion-icon slot="icon-only" :icon="passwordVisible ? ionicons.eyeOffOutline : ionicons.eyeOutline"/>
+								</ion-button>
+							</ion-item>
+							<div class="ion-margin-top">
+								<loadingBtn color="primary" expand="block" type="submit" :loading="loading" :text="$t('actions.submit')" :loadingText="$t('actions.submitting')" class="ion-margin-bottom"/>
+								<p v-if="error !== {}" class="text-danger">{{ error.message }}</p>
+								<hr class="ion-margin-vertical"/>
+								<p>
+									<router-link :to="{name: 'SignIn'}">{{ $t('views.auth.backToSignIn') }}</router-link>
+								</p>
+								<!--								<localeDropdown/>-->
+							</div>
+						</form>
+					</ion-card-content>
+				</ion-card>
+			</ion-col>
+		</ion-row>
+	</ion-grid>
 </template>
 <script>
+	import {
+		IonCard,
+		IonCardHeader,
+		IonCardContent,
+		IonCardTitle,
+		IonIcon,
+		IonItem,
+		IonLabel,
+		IonButton,
+		IonInput,
+		IonGrid,
+		IonRow,
+		IonCol,
+	} from '@ionic/vue';
+	import { eyeOutline, eyeOffOutline, keyOutline, personOutline } from 'ionicons/icons';
+
 	import { mapActions } from 'vuex';
-	import localeDropdown from '@/components/structure/localeDropdown';
+	// import localeDropdown from '@/components/structure/localeDropdown';
 	import loadingBtn from '@/components/structure/loadingBtn';
 
 	export default {
 		name: 'forceChangePassword',
 		components: {
-			localeDropdown,
+			IonCard,
+			IonCardHeader,
+			IonCardContent,
+			IonCardTitle,
+			IonIcon,
+			IonItem,
+			IonLabel,
+			IonButton,
+			IonInput,
+			IonGrid,
+			IonRow,
+			IonCol,
+			// localeDropdown,
 			loadingBtn,
 		},
 		props: {
@@ -47,6 +84,12 @@
 				error: {},
 				newPassword: '',
 				passwordVisible: false,
+				ionicons: {
+					personOutline,
+					keyOutline,
+					eyeOutline,
+					eyeOffOutline,
+				},
 			};
 		},
 		methods: {
@@ -62,6 +105,7 @@
 						old_password: this.credentials.password,
 						new_password: this.newPassword,
 					});
+					await this.$router.push({name: 'SignIn'});
 					this.loading = false;
 				} catch (error) {
 					this.error = error;
