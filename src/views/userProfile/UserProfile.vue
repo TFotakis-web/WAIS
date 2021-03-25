34baf785d25411c7fc3c2035dde80b086e983703
TFotakis <fotakistzanis@gmail.com>
2021-03-25 19:25:50 +0200

    UserProfile.vue: Migrated to Vue3 & Ionic

--- src/views/userProfile/UserProfile.vue
+++ src/views/userProfile/UserProfile.vue
@@ -1,17 +1,23 @@
 <template>
-	<div class="pt-5">
-		<h5 class="text-info text-center mb-5">{{ $t('various.underConstruction') }}</h5>
-		<mdb-container>
-			<personal-info-card/>
-		</mdb-container>
-	</div>
+	<base-layout :page-title="$t('views.userProfile.pageTitle')">
+		<h1 class="ion-text-center">{{ $t('various.underConstruction') }}</h1>
+		<ion-grid fixed>
+			<ion-row>
+				<ion-col>
+					<personal-info-card/>
+				</ion-col>
+			</ion-row>
+		</ion-grid>
+	</base-layout>
 </template>
 <script>
+	import BaseLayout from '@/components/base/BaseLayout';
 	import PersonalInfoCard from '@/views/userProfile/personalInfoCard';
 
 	export default {
 		name: 'UserProfile',
 		components: {
+			BaseLayout,
 			PersonalInfoCard,
 		},
 	};
