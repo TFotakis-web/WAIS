0338f8378a9b70c302d92a3ce24c5cba42d80bd9
TFotakis <fotakistzanis@gmail.com>
2021-03-26 19:31:40 +0200

    pageStructure.js: Added backButtonDefaultHref state, mutation and getter

--- src/plugins/store/pageStructure.js
+++ src/plugins/store/pageStructure.js
@@ -3,18 +3,26 @@ export const pageStructure = {
 	state: {
 		pageTitle: '',
 		pageBackButton: false,
+		backButtonDefaultHref: '/',
+		metaTitle: document.querySelector('head title')
 	},
 	mutations: {
 		setPageTitle(state, payload) {
 			state.pageTitle = payload;
+			const tabTitle = payload ? `${payload} | WAIS` : 'WAIS';
+			state.metaTitle.textContent = tabTitle;
 		},
 		setPageBackButton(state, payload) {
 			state.pageBackButton = payload;
+		},
+		setBackButtonDefaultHref(state, payload) {
+			state.backButtonDefaultHref = payload;
 		}
 	},
 	actions: {},
 	getters: {
 		pageTitle: (state) => state.pageTitle,
 		pageBackButton: (state) => state.pageBackButton,
+		backButtonDefaultHref: (state) => state.backButtonDefaultHref,
 	},
 };
