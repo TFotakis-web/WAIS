7023c8f77dfcf8487ae1a24a4fc30d0535443d47
TFotakis <fotakistzanis@gmail.com>
2021-03-27 00:44:53 +0200

    Removed position absolute on "Under Construction" header

--- src/views/processing/DueDateRegister.vue
+++ src/views/processing/DueDateRegister.vue
@@ -1,5 +1,5 @@
 <template>
-	<h1 style="text-align: center; position: absolute; left: 0; right: 0; top: 50%; transform: translateY(-50%);">{{ $t('various.underConstruction') }}</h1>
+	<h1 class="ion-text-center">{{ $t('various.underConstruction') }}</h1>
 </template>
 <script>
 	export default {
