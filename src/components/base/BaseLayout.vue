8d3ca56e2fb857ad551af3b0547dbaed0f423f9a
TFotakis <fotakistzanis@gmail.com>
2021-03-26 19:32:14 +0200

    BaseLayout.vue: Added backButtonDefaultHref prop on toolbar

--- src/components/base/BaseLayout.vue
+++ src/components/base/BaseLayout.vue
@@ -2,7 +2,7 @@
 	<IonSplitPane content-id="main-content">
 		<side-navigation/>
 		<ion-page id="main-content">
-			<toolbar :page-title="$store.getters['pageStructure/pageTitle']" :show-back-button="$store.getters['pageStructure/pageBackButton']"/>
+			<toolbar :page-title="$store.getters['pageStructure/pageTitle']" :show-back-button="$store.getters['pageStructure/pageBackButton']" :back-button-default-href="$store.getters['pageStructure/backButtonDefaultHref']"/>
 			<ion-content :fullscreen="true">
 				<router-view/>
 			</ion-content>
