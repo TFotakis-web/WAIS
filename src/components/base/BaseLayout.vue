62e75181a61fdb591335891199ef5351e565522b
TFotakis <fotakistzanis@gmail.com>
2021-03-27 00:45:20 +0200

    BaseLayout.vue: Added router-view transitions

--- src/components/base/BaseLayout.vue
+++ src/components/base/BaseLayout.vue
@@ -4,7 +4,11 @@
 		<ion-page id="main-content">
 			<toolbar :page-title="$store.getters['pageStructure/pageTitle']" :show-back-button="$store.getters['pageStructure/pageBackButton']" :back-button-default-href="$store.getters['pageStructure/backButtonDefaultHref']"/>
 			<ion-content :fullscreen="true">
-				<router-view/>
+				<router-view v-slot="{ Component }">
+					<transition name="route" mode="out-in">
+						<component :is="Component"/>
+					</transition>
+				</router-view>
 			</ion-content>
 		</ion-page>
 	</IonSplitPane>
@@ -22,8 +26,28 @@
 		props: {
 			pageTitle: {
 				type: String,
-				default: ''
-			}
-		}
+				default: '',
+			},
+		},
 	};
 </script>
+<style>
+	.route-enter-from {
+		opacity: 0;
+		transform: translateY(5px) scale(0.995);
+		zoom: 0.5;
+	}
+
+	.route-enter-active {
+		transition: all 200ms ease-out;
+	}
+
+	.route-leave-active {
+		transition: all 200ms ease-in;
+	}
+
+	.route-leave-to {
+		opacity: 0;
+		transform: translateY(5px) scale(0.995);
+	}
+</style>
