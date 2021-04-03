efc8a5eb3187b89585f42d827ff4835945d2f08d
TFotakis <fotakistzanis@gmail.com>
2021-04-03 20:44:26 +0300

    SideNavigationCategory.vue: Added support for routes with thumbnails

--- src/components/base/SideNavigationCategory.vue
+++ src/components/base/SideNavigationCategory.vue
@@ -1,19 +1,31 @@
 <template>
-	<ion-item v-if="routeCategory.children" button @click="shown = !shown" detail="false" class="topLevelRoute">
-		<ion-icon :icon="routeCategory.icon" slot="start"/>
-		<ion-label>{{ routeCategory.name }}</ion-label>
-		<ion-icon :icon="shown ? $ionicons.chevronDownOutline : $ionicons.chevronForwardOutline" slot="end"/>
+	<ion-item v-if="routeCategory.thumbnail" :router-link="routeCategory.to" button detail="false" :class="{ selected: isSelected(routeCategory) }" class="topLevelRoute">
+		<ion-thumbnail slot="start">
+			<ion-img :src="routeCategory.thumbnail"/>
+		</ion-thumbnail>
+		<ion-label>
+			<h1 v-if="routeCategory.h1">{{ routeCategory.h1 }}</h1>
+			<h2 v-if="routeCategory.h2">{{ routeCategory.h2 }}</h2>
+			<p v-if="routeCategory.p">{{ routeCategory.p }}</p>
+		</ion-label>
 	</ion-item>
-	<ion-item v-else :router-link="routeCategory.to" button detail="false" :class="{ selected: isSelected(routeCategory) }" class="topLevelRoute">
-		<ion-icon :icon="routeCategory.icon" slot="start"/>
-		<ion-label>{{ routeCategory.name }}</ion-label>
-	</ion-item>
-	<ion-list v-if="shown && routeCategory.children">
-		<ion-item v-for="(route, i) in routeCategory.children" :key="i" button router-direction="root" :router-link="route.to" lines="none" detail="false" class="hydrated" :class="{ selected: isSelected(route) }">
-			<ion-icon slot="start"/>
-			<ion-label>{{ route.name }}</ion-label>
+	<template v-else>
+		<ion-item v-if="routeCategory.children" button @click="shown = !shown" detail="false" class="topLevelRoute">
+			<ion-icon :icon="routeCategory.icon" slot="start"/>
+			<ion-label>{{ routeCategory.name }}</ion-label>
+			<ion-icon :icon="shown ? $ionicons.chevronDownOutline : $ionicons.chevronForwardOutline" slot="end"/>
 		</ion-item>
-	</ion-list>
+		<ion-item v-else :router-link="routeCategory.to" button detail="false" :class="{ selected: isSelected(routeCategory) }" class="topLevelRoute">
+			<ion-icon :icon="routeCategory.icon" slot="start"/>
+			<ion-label>{{ routeCategory.name }}</ion-label>
+		</ion-item>
+		<ion-list v-if="shown && routeCategory.children">
+			<ion-item v-for="(route, i) in routeCategory.children" :key="i" button router-direction="root" :router-link="route.to" lines="none" detail="false" class="hydrated" :class="{ selected: isSelected(route) }">
+				<ion-icon slot="start"/>
+				<ion-label>{{ route.name }}</ion-label>
+			</ion-item>
+		</ion-list>
+	</template>
 </template>
 <script>
 	export default {
