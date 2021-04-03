1ea92fdc56205ab339b03a53a0180f7016da1cb4
TFotakis <fotakistzanis@gmail.com>
2021-04-03 22:57:51 +0300

    createOfficeCard.vue: Created & Implemented

---
+++ src/views/home/createOfficeCard.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<span>{{ $t('views.homePage.createOfficeCard.youHaveNoConnectedOffices') + ' ' }}</span>
+		<router-link :to="{ name: 'OfficeCreationForm' }">{{ $t('views.homePage.createOfficeCard.createOne') }}</router-link>
+		<span>{{ ' ' + $t('views.homePage.createOfficeCard.orCheckYour') + ' ' }}</span>
+		<router-link :to="{ name: 'Notifications' }">{{ $t('components.navigation.navbar-item.notifications') }}</router-link>
+		<span>{{ ' ' + $t('views.homePage.createOfficeCard.forInvitations') }}</span>
+	</div>
+</template>
+<script>
+	export default {
+		name: 'createOfficeCard',
+	};
+</script>
+<style scoped>
+</style>
