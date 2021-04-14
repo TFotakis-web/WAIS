19534fc58055aa1ae679d868c364bf68820f12a6
TFotakis <fotakistzanis@gmail.com>
2021-04-15 00:35:29 +0300

    ManageUser.vue: Created & Initialized

---
+++ src/views/office/ManageUser.vue
@@ -0,0 +1,21 @@
+<template>
+	<ion-grid fixed>
+		<h1 class="ion-text-center">{{ $t('various.underConstruction') }}</h1>
+		<h1 class="ion-text-center">{{ username }}</h1>
+	</ion-grid>
+</template>
+<script>
+	export default {
+		name: 'ManageUser',
+		mounted() {
+			this.$store.commit('pageStructure/setPageTitle', () => window.vm.$t('views.Office.manageUser.pageTitle'));
+			this.$store.commit('pageStructure/setPageBackButton', true);
+		},
+		computed: {
+			username() {
+				return this.$route.params.username;
+			},
+		},
+	};
+</script>
+
