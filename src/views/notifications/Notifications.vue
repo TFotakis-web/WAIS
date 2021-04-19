b841f96f78f980dfda8fd12835ebed56bd160337
TFotakis <fotakistzanis@gmail.com>
2021-04-19 22:47:05 +0300

    Notifications.vue: Added all requests on notifications list

--- src/views/notifications/Notifications.vue
+++ src/views/notifications/Notifications.vue
@@ -1,19 +1,25 @@
 <template>
 	<ion-grid fixed>
-		<ion-card>
-			<ion-card-header>
-				<ion-card-title>Create Office requests</ion-card-title>
-			</ion-card-header>
-			<ion-card-content>
-				<ion-list>
-					<ion-item v-for="request in $store.getters['request/requestsForNewOfficeReceived']" :key="request.id" @click="$router.push({name: 'AdminNotificationDetails', params: {id: request.id}})" button>
-						<strong>{{ request.payload.createOfficePayload.officeName }}</strong>
-<!--						<strong>{{ request.payload.createOfficePayload.officeName }}: </strong>-->
-<!--						<span>{{ request.payload.createOfficePayload.family_name + ' ' + request.payload.createOfficePayload.name + ' ' + request.payload.createOfficePayload.fathersName }}</span>-->
-					</ion-item>
-				</ion-list>
-			</ion-card-content>
-		</ion-card>
+		<ion-list>
+			<ion-item v-for="request in $store.getters['request/requests']" :key="request.id" @click="$router.push({name: $store.getters['auth/isAdmin'] ? 'AdminNotificationDetails' : 'OfficeCreationForm', params: {id: request.id}})" button>
+				<ion-label v-if="request.type === 'CREATE_OFFICE'">
+					<h3>{{ $t('requests.createOffice') }}</h3>
+					<p>{{ request.payload.createOfficePayload.officeName }} • {{ request.senderUsername }}</p>
+				</ion-label>
+				<ion-label v-if="request.type === 'CREATE_OFFICE_CONNECTION'">
+					<h3>{{ $t('requests.createOfficeConnection') }}</h3>
+					<p>{{ request.senderUsername }} • {{ request.senderEmail }}</p>
+				</ion-label>
+				<ion-label v-if="request.type === 'INVITE_EMPLOYEE_TO_OFFICE'">
+					<h3>{{ $t('requests.inviteEmployeeToOffice') }}</h3>
+					<p>{{ request.senderUsername }} • {{ request.senderEmail }}</p>
+				</ion-label>
+				<ion-label v-if="request.type === 'INVITE_CONTRACTOR_TO_OFFICE'">
+					<h3>{{ $t('requests.inviteContractorToOffice') }}</h3>
+					<p>{{ request.senderUsername }} • {{ request.senderEmail }}</p>
+				</ion-label>
+			</ion-item>
+		</ion-list>
 	</ion-grid>
 </template>
 <script>
