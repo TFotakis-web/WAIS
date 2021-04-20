e66f758ffa27a12bc6b3cb8bdf614559ae3d70af
TFotakis <fotakistzanis@gmail.com>
2021-04-20 15:24:55 +0300

    inviteContractorToOfficeNotification.vue: Created & Initialized

---
+++ src/views/notifications/inviteContractorToOfficeNotification.vue
@@ -0,0 +1,14 @@
+<template>
+	<ion-item @click="$router.push({name: 'InviteContractorToOfficeNotificationDetails', params: {id: request.id}})" button>
+		<ion-label>
+			<h3>{{ $t('requests.inviteContractorToOffice') }}</h3>
+			<p>{{ request.senderUsername }} • {{ request.senderEmail }}</p>
+		</ion-label>
+	</ion-item>
+</template>
+<script>
+	export default {
+		name: 'inviteContractorToOfficeNotification',
+		props: ['request'],
+	};
+</script>
