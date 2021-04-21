76dd218f4bae79c2236b1196f73412f359a78112
TFotakis <fotakistzanis@gmail.com>
2021-04-21 14:50:40 +0300

    inviteEmployeeToOfficeItem.vue: Created & Initialized

---
+++ src/views/office/inviteEmployeeToOfficeItem.vue
@@ -0,0 +1,15 @@
+<template>
+	<ion-item @click="$router.push({name: 'InviteEmployeeToOfficeItemDetails', params: {id: request.id}})" button>
+		<ion-label>
+			<h3>{{ $t('requests.inviteEmployeeToOffice') }}</h3>
+			<p>{{ request.senderUsername }} • {{ request.senderEmail }}</p>
+		</ion-label>
+		<ion-badge color="warning">Pending</ion-badge>
+	</ion-item>
+</template>
+<script>
+	export default {
+		name: 'inviteEmployeeToOfficeItem',
+		props: ['request'],
+	};
+</script>
