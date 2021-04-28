4c903ff981f25485671ccadea5286d36f3ab1866
TFotakis <fotakistzanis@gmail.com>
2021-04-28 13:02:25 +0300

    S3Object.vue: Created & Implemented

---
+++ src/components/structure/S3Object.vue
@@ -0,0 +1,43 @@
+<template>
+	<ion-button :href="url" target="_blank" fill="clear" size="small">{{ s3Object.filename }}</ion-button>
+	<ion-button v-if="disableDelete" @click="deleteFile()" fill="clear" size="small">
+		<ion-icon :icon="$ionicons.closeOutline" slot="icon-only"/>
+	</ion-button>
+</template>
+<script>
+	import { Storage } from 'aws-amplify';
+
+	export default {
+		name: 'S3Object',
+		props: [
+			's3Object',
+			'defaultUrl',
+			'disableDelete'
+		],
+		emits: ['fileDeleted'],
+		data() {
+			return {
+				url: '',
+			};
+		},
+		methods: {
+			async deleteFile() {
+				await Storage.remove(this.s3Object.filePath + '/' + this.s3Object.filename, { level: this.s3Object.level });
+				this.$emit('fileDeleted');
+			},
+		},
+		watch: {
+			s3Object: {
+				immediate: true,
+				handler: async function(newValue) {
+					if (!newValue || !newValue.filePath || !newValue.filename || !newValue.level) {
+						this.url = this.defaultUrl;
+						return;
+					}
+					const url = await Storage.get(newValue.filePath + '/' + newValue.filename, { level: newValue.level });
+					this.url = url ? url : this.defaultUrl;
+				},
+			},
+		},
+	};
+</script>
