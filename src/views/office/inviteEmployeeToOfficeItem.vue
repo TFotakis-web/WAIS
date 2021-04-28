7898a3e53a7611efcc11cb9521535eef0f3037dc
TFotakis <fotakistzanis@gmail.com>
2021-04-28 11:55:07 +0300

    inviteEmployeeToOfficeItem.vue: Changed subtitle

--- src/views/office/inviteEmployeeToOfficeItem.vue
+++ src/views/office/inviteEmployeeToOfficeItem.vue
@@ -2,7 +2,8 @@
 	<ion-item @click="$router.push({name: 'InviteEmployeeToOfficeItemDetails', params: {id: request.id}})" button>
 		<ion-label>
 			<h3>{{ $t('requests.inviteEmployeeToOffice') }}</h3>
-			<p>{{ request.senderUsername }} • {{ request.senderEmail }}</p>
+			<p>{{ request.receiverEmail }}</p>
+<!--			<p>{{ request.senderUsername }} • {{ request.senderEmail }}</p>-->
 		</ion-label>
 		<ion-badge color="warning">Pending</ion-badge>
 	</ion-item>
