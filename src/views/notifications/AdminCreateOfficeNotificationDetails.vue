dd3843bc6f65595134598f0e3d4cbe27943c7336
TFotakis <fotakistzanis@gmail.com>
2021-05-11 12:23:20 +0300

    AdminCreateOfficeNotificationsDetails.vue: Implemented ion-input-item

--- src/views/notifications/AdminCreateOfficeNotificationDetails.vue
+++ src/views/notifications/AdminCreateOfficeNotificationDetails.vue
@@ -37,35 +37,11 @@
 		<ion-button v-for="file in request.payload.createOfficePayload.files" :key="file.level + '/' + file.idToken + '/' + file.filePath + '/' + file.filename" @click="openFile(file)" fill="clear">{{ file.filename }}</ion-button>
 		<form @submit.prevent="acceptRequest">
 			<ion-list>
-				<ion-item>
-					<ion-icon :icon="$ionicons.calendarOutline" slot="start" class="ion-align-self-center"/>
-					<ion-label position="floating">{{ $t('fields.subscriptionExpirationDate') }}</ion-label>
-					<ion-datetime-custom v-model="form.subscriptionExpirationDate" :disable-past="true" name="subscriptionExpirationDate" :required="true" :return-date="true"/>
-				</ion-item>
-				<ion-item>
-					<ion-icon :icon="$ionicons.peopleOutline" slot="start" class="ion-align-self-center"/>
-					<ion-label position="floating">{{ $t('fields.partnersNumberLimit') }}</ion-label>
-					<ion-input v-model="form.partnersNumberLimit" :min="0" type="number" name="partnersNumberLimit" required class="no-arrows"/>
-				</ion-item>
-				<ion-item>
-					<ion-icon slot="start"/>
-					<ion-label position="floating">{{ $t('fields.employeesNumberLimit') }}</ion-label>
-					<ion-input v-model="form.employeesNumberLimit" :min="0" type="number" name="employeesNumberLimit" required class="no-arrows"/>
-				</ion-item>
-				<ion-item>
-					<ion-icon :icon="$ionicons.appsOutline" slot="start" class="ion-align-self-center"/>
-					<ion-label position="floating">{{ $t('fields.featureAccess') }}</ion-label>
-					<ion-select v-model="featureAccess" multiple :ok-text="$t('actions.ok')" :cancel-text="$t('actions.cancel')" required>
-						<ion-select-option v-for="o in featureAccessOptions" :key="o.text" :value="o.value">{{ o.text }}</ion-select-option>
-					</ion-select>
-				</ion-item>
-				<ion-item>
-					<ion-icon :icon="$ionicons.cubeOutline" slot="start" class="ion-align-self-center"/>
-					<ion-label position="floating">{{ $t('fields.companyAccess') }}</ion-label>
-					<ion-select v-model="companyAccess" multiple :ok-text="$t('actions.ok')" :cancel-text="$t('actions.cancel')" required>
-						<ion-select-option v-for="o in companyOptions" :key="o.text" :value="o.value">{{ o.text }}</ion-select-option>
-					</ion-select>
-				</ion-item>
+				<ion-input-item v-model="form.subscriptionExpirationDate" :config="$inputConfigs.subscriptionExpirationDate"/>
+				<ion-input-item v-model="form.partnersNumberLimit" :config="$inputConfigs.partnersNumberLimit"/>
+				<ion-input-item v-model="form.employeesNumberLimit" :config="$inputConfigs.employeesNumberLimit"/>
+				<ion-input-item v-model="featureAccess" :config="$inputConfigs.featureAccess"/>
+				<ion-input-item v-model="companyAccess" :config="$inputConfigs.companyAccess"/>
 			</ion-list>
 			<div class="ion-text-center">
 				<loading-btn color="success" type="submit" :loading="acceptLoading" :text="$t('actions.accept')" :loadingText="$t('actions.accepting')"/>
@@ -75,17 +51,17 @@
 	</ion-grid>
 </template>
 <script>
+	import IonInputItem from '@/components/structure/ionInputItem';
 	import { mapActions, mapGetters } from 'vuex';
-	// import { Storage } from 'aws-amplify';
 	import { API, graphqlOperation } from 'aws-amplify';
 	import { getS3Object } from '@/graphql/custom-queries';
 	import loadingBtn from '@/components/structure/loadingBtn';
-	import IonDatetimeCustom from '@/components/structure/ionDatetimeCustom';
+
 
 	export default {
 		name: 'NotificationDetails',
 		components: {
-			IonDatetimeCustom,
+			IonInputItem,
 			loadingBtn,
 		},
 		data() {
@@ -198,6 +174,7 @@
 				this.resolveRequest({ id: this.request.id, decision: 'ACCEPT', payload: { createOfficePayload: payload } })
 					.then(() => {
 						this.$toast.saveSuccess();
+						this.$mitt.emit('markInputClean:all');
 						this.$router.push({ name: 'AdminNotifications' });
 					})
 					.catch((err) => this.$toast.error(err))
@@ -208,6 +185,7 @@
 				this.resolveRequest({ id: this.request.id, decision: 'REJECT' })
 					.then(() => {
 						this.$toast.saveSuccess();
+						this.$mitt.emit('markInputClean:all');
 						this.$router.push({ name: 'AdminNotifications' });
 					})
 					.catch((err) => this.$toast.error(err))
