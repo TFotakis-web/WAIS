fa75c642457e19e584bcf89890fba373318785ed
TFotakis <fotakistzanis@gmail.com>
2021-05-11 11:17:51 +0300

    ionInputItem.vue: Added inputGroup as prop

--- src/components/structure/ionInputItem.vue
+++ src/components/structure/ionInputItem.vue
@@ -2,7 +2,7 @@
 	<ion-item>
 		<ion-icon v-if="config.icon || config.iconPlaceholder" :icon="config.icon" slot="start" class="ion-align-self-center"/>
 		<ion-label v-if="config.label" :position="config.slot !== 'start' ? 'floating' : undefined">{{ config.label() }}</ion-label>
-		<ion-input-custom v-model="modelValue" :config="config" @update:modelValue="el => $emit('update:modelValue', el)"/>
+		<ion-input-custom v-model="modelValue" :config="config" @update:modelValue="el => $emit('update:modelValue', el)" :inputGroup="inputGroup"/>
 	</ion-item>
 </template>
 <script>
@@ -10,7 +10,7 @@
 	export default {
 		name: 'ionInputItem',
 		components: { IonInputCustom },
-		props: ['modelValue', 'config'],
+		props: ['modelValue', 'config', 'inputGroup'],
 		emits: ['update:modelValue'],
 	};
 </script>
