a4c58c20a5fc2190ef3d183c089f8ed38ad61520
TFotakis <fotakistzanis@gmail.com>
2021-05-11 16:05:55 +0300

    inviteOfficeItem.vue: Created & Implemented

---
+++ src/views/office/inviteOfficeItem.vue
@@ -0,0 +1,15 @@
+<template>
+	<ion-item @click="$router.push({name: 'InviteOfficeItemDetails', params: {id: request.id}})" button>
+		<ion-label>
+			<h3>{{ $t('requests.inviteOffice') }}</h3>
+			<p>{{ request.receiverEmail }}</p>
+		</ion-label>
+		<ion-badge color="warning">{{ $t('actions.pending') }}</ion-badge>
+	</ion-item>
+</template>
+<script>
+	export default {
+		name: 'inviteOfficeItem',
+		props: ['request'],
+	};
+</script>
