cde1c584d12349227589174b09e90f42d85385e7
TFotakis <fotakistzanis@gmail.com>
2021-05-16 17:49:16 +0300

    Partners.vue: Created & Implemented

---
+++ src/views/partners/Partners.vue
@@ -0,0 +1,19 @@
+<template>
+	<ion-grid fixed>
+		<h1 class="ion-text-center">{{ $t('various.underConstruction') }}</h1>
+		<partner-management-card/>
+	</ion-grid>
+</template>
+<script>
+	import PartnerManagementCard from '@/views/partners/partnerManagementCard';
+
+
+	export default {
+		name: 'Partners',
+		components: { PartnerManagementCard },
+		mounted() {
+			this.$store.commit('pageStructure/setPageTitle', () => window.vm.$t('views.partners.pageTitle'));
+			this.$store.commit('pageStructure/setPageBackButton', false);
+		},
+	};
+</script>
