8813c66c4c42c009d69ccdebef6a8135df3e55b6
TFotakis <fotakistzanis@gmail.com>
2021-05-16 17:49:34 +0300

    PartnerDetails.vue: Created & Initialized

---
+++ src/views/partners/PartnerDetails.vue
@@ -0,0 +1,13 @@
+<template>
+	<h1 class="ion-text-center">{{ $t('various.underConstruction') }}</h1>
+</template>
+<script>
+	export default {
+		name: 'PartnerDetails',
+		mounted() {
+			this.$store.commit('pageStructure/setPageTitle', () => window.vm.$t('views.partnerDetails.pageTitle'));
+			this.$store.commit('pageStructure/setPageBackButton', true);
+			this.$store.commit('pageStructure/setBackButtonDefaultHref', this.$router.resolve({ name: 'Partners' }).fullPath);
+		},
+	};
+</script>
