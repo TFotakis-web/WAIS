1b8a134c26efb2fc3263d46027015f302bc26da1
TFotakis <fotakistzanis@gmail.com>
2021-05-16 18:40:47 +0300

    Office.vue: Replaced user-management-table-card with employee-management-card

--- src/views/office/Office.vue
+++ src/views/office/Office.vue
@@ -7,30 +7,21 @@
 		</ion-row>
 		<ion-row>
 			<ion-col>
-				<!--				<user-permissions-table-card/>-->
-				<user-management-table-card/>
+				<employee-management-card/>
 			</ion-col>
 		</ion-row>
-		<ion-row>
-		<ion-col>
-			<office-management-table-card/>
-		</ion-col>
-	</ion-row>
 	</ion-grid>
 </template>
 <script>
+	import EmployeeManagementCard from '@/views/office/employeeManagementCard';
 	import officeInfoCard from '@/views/office/officeInfoCard';
-	// import UserPermissionsTableCard from '@/views/office/userPermissionsTableCard';
-	import UserManagementTableCard from '@/views/office/userManagementTableCard';
-	import OfficeManagementTableCard from '@/views/office/officeManagementTableCard';
+
 
 	export default {
 		name: 'Office',
 		components: {
+			EmployeeManagementCard,
 			officeInfoCard,
-			// UserPermissionsTableCard,
-			UserManagementTableCard,
-			OfficeManagementTableCard,
 		},
 		mounted() {
 			this.$store.commit('pageStructure/setPageTitle', () => window.vm.$t('views.Office.pageTitle'));
